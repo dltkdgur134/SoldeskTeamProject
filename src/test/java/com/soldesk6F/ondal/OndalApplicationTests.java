a509827ddb593bfdbb462ebcc1e541abf3ca867c
CORE <junyoung070143@gmail.com>
2025-03-27 17:24:37 +0900

    ondal 1.0

---
+++ src/test/java/com/soldesk6F/ondal/OndalApplicationTests.java
@@ -0,0 +1,14 @@
+package com.soldesk6F.ondal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OndalApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
