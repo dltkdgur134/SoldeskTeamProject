2e8a5d636de833c189c9ca985cc2443924e05e1b
dltkdgur134164@gmail.com <dltkdgur134164@gmail.com>
2025-04-03 10:08:24 +0900

    rider Entity 추가

---
+++ src/main/java/com/soldesk6F/ondal/rider/Rider.java
@@ -0,0 +1,39 @@
+package com.soldesk6F.ondal.rider;
+
+import java.util.UUID;
+
+import org.hibernate.annotations.UuidGenerator;
+
+import com.soldesk6F.ondal.user.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Rider {
+	@Id
+	@GeneratedValue
+	@UuidGenerator
+	@Column( updatable = false, nullable = false, unique = true)
+	private UUID riderId;
+	
+	@OneToOne
+	@JoinColumn(name = "user_id", nullable = false, unique =true)
+	private User userId;
+	
+	@Column(name = "secondary_password" , nullable = false)
+	private String secondart_Password;
+	
+	
+	
+}
