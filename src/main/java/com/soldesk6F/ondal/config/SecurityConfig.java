04d52c3fb0ef98a59b3e151aab253ea90559fbfa
AHN <idiocy1009@naver.com>
2025-04-08 14:15:43 +0900

    머지

--- src/main/java/com/soldesk6F/ondal/config/SecurityConfig.java
+++ src/main/java/com/soldesk6F/ondal/config/SecurityConfig.java
@@ -51,36 +51,49 @@ public class SecurityConfig {
                 .build();
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests(auth -> auth
-            		.requestMatchers("/**", "/alert", "/login/**", "/register/**", "/css/**", "/js/**").permitAll()
-//            	    .requestMatchers("/admin/**").hasRole(Role.OWNER)
-//            	    .requestMatchers("/")
-            	    .anyRequest().authenticated()
-            )
-            .formLogin(form -> form
-                .loginPage("/login/tryLogin")
-                .defaultSuccessUrl("/", true)
-                .permitAll()
-            )
-            .logout(logout -> logout		
-                .logoutSuccessUrl("/")
-            ).oauth2Login(oauth2 -> oauth2
-                    .loginPage("/login/tryOAuthLogin") // 커스텀 로그인 페이지 설정
-                    .userInfoEndpoint(userInfo -> userInfo
-                    .userService(customOAuth2UserService) // 사용자 정보 처리
-                    )
-                );
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    	http
+    		.authorizeHttpRequests(auth -> auth
+    			.requestMatchers("/**","/register", "/login/**", "/css/**", "/js/**",  "/img/**").permitAll() 
+    			.anyRequest().authenticated() 
+				)
+			.formLogin(form -> form
+				.loginPage("/login/tryLogin")           // 우리가 만든 로그인 페이지
+				.loginProcessingUrl("/login")  // 로그인 form의 action 주소 (POST)
+				.defaultSuccessUrl("/",true)        // 로그인 성공 후 이동할 주소
+				.failureUrl("/login?error")    // 로그인 실패 시 주소
+				.permitAll()
+			)
+			.logout(logout -> logout
+				.logoutUrl("/logout")
+				.logoutSuccessUrl("/login?logout")
+				.permitAll()
+			)
+			.csrf(csrf -> csrf.disable());
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//.authorizeHttpRequests(auth -> auth // 임시 로그인 비활성화
+		//.anyRequest().permitAll() // 모든 요청 허용
+		//)
+		//.formLogin(form -> form.disable()) // 기본 로그인 페이지 비활성화
+		//.logout(logout -> logout.disable()) // 로그아웃도 비활성화
+		//.csrf(csrf -> csrf.disable()); // CSRF 비활성화
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        
         return http.build();
     }
     
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        // 실제 서비스에서는 절대 사용 금지. 테스트 용도로만!
-        return NoOpPasswordEncoder.getInstance();
-    }
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        // 실제 서비스에서는 절대 사용 금지. 테스트 용도로만!
+//        return NoOpPasswordEncoder.getInstance();
+//    }
+    
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
     
 }
