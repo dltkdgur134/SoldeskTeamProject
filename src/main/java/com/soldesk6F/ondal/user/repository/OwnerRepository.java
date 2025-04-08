d3aeddbcea82c4ab52c62de7d330708518a67661
dltkdgur134164@gmail.com <dltkdgur134164@gmail.com>
2025-04-08 17:38:54 +0900

    commit yet

--- src/main/java/com/soldesk6F/ondal/user/repository/OwnerRepository.java
+++ src/main/java/com/soldesk6F/ondal/user/repository/OwnerRepository.java
@@ -1,27 +1,11 @@
 package com.soldesk6F.ondal.user.repository;
 
-<<<<<<< HEAD
 import com.soldesk6F.ondal.user.entity.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface OwnerRepository extends JpaRepository<Owner, UUID> {
-
-    // userId를 통해 Owner를 찾고 싶을 때
-    Optional<Owner> findByUser_UserId(String userId);
-
-    // user의 id가 특정 값인지 확인
-    boolean existsByUser_UserId(String userId);
-=======
-import java.util.Optional;
-import java.util.UUID;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.soldesk6F.ondal.user.entity.Owner;
-import com.soldesk6F.ondal.user.entity.User;
 
 public interface OwnerRepository extends JpaRepository<Owner, UUID> {
 
@@ -30,5 +14,4 @@ public interface OwnerRepository extends JpaRepository<Owner, UUID> {
 		
 	
 	
->>>>>>> origin/user.register+login
 }
