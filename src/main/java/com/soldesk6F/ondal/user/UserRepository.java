6b0ec2fb43d3a20d8daa54ff2b7441b8e9467f13
rien0885 <rien0885@gmail.com>
2025-04-08 11:03:14 +0900

    마이페이지 수정

--- src/main/java/com/soldesk6F/ondal/user/UserRepository.java
+++ src/main/java/com/soldesk6F/ondal/user/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, String> {
 	boolean existsById(String userId);
     boolean existsByEmail(String email); // 이메일 중복 체크용
+    
 }
