e028d095bf13f69ea256ff8292e0327e8b421c59
dltkdgur134164@gmail.com <dltkdgur134164@gmail.com>
2025-04-15 10:18:12 +0900

    1

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,14 +1,8 @@
-window.toggleRoleLogos = function () {
-    const logos = document.querySelectorAll('.role-logo');
-    logos.forEach(logo => {
-        logo.classList.toggle('show');
-    });
-};
+
 $(function () {
 	$(".nav-item > .active").css("color", "red");
 	
 	$(".nav-link").click(function (){
-		alert('test');
 		$(".nav-item > .active").css("color", "#ffc107;");
 		$(".nav-link").removeClass('active');
 		
