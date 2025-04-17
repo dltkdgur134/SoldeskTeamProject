79650e8ad1e4d573bef6d4e08efd160a92086ddd
dltkdgur134164@gmail.com <dltkdgur134164@gmail.com>
2025-04-17 17:55:22 +0900

    owner info page 약간 수정 및 ㅊ추가

---
+++ src/main/java/com/soldesk6F/ondal/user/controller/owner/UpdateOwnerController.java
@@ -0,0 +1,46 @@
+package com.soldesk6F.ondal.user.controller.owner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.soldesk6F.ondal.login.CustomUserDetails;
+import com.soldesk6F.ondal.user.entity.Owner;
+import com.soldesk6F.ondal.user.entity.Rider;
+import com.soldesk6F.ondal.user.repository.OwnerRepository;
+import com.soldesk6F.ondal.user.service.OwnerService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class UpdateOwnerController {
+	private final OwnerRepository ownerRepository;
+	private final OwnerService ownerService;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder; // 필드 주입
+
+	@GetMapping("owner/ownerMypage")
+    public String showEditPage(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
+		String userId = userDetails.getUser().getUserId();
+		Owner owner = ownerService.getOwnerByUserId(userId);
+		model.addAttribute("owner", owner);
+		
+		return "content/owner/ownerMypage"; 
+    }
+	
+	@GetMapping("owner/updateOwnerInfo")
+	public String showUpdateRiderInfoForm(@AuthenticationPrincipal CustomUserDetails userDetails, Model model) {
+		String userId = userDetails.getUser().getUserId();
+		Owner owner = ownerService.getOwnerByUserId(userId);
+		model.addAttribute("owner",owner);
+		return "content/owner/ownerUpdateInfo"; // 수정 폼 페이지
+	}
+	
+	
+	
+}
