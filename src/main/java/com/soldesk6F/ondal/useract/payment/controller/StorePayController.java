a9809ea1f323c3c36b02fc17b8395f0c327f872b
Kwak Junyoung <junyoung070143  @gmail  .com>
2025-04-30 17:23:36 +0900

    PG payment not fin

---
+++ src/main/java/com/soldesk6F/ondal/useract/payment/controller/StorePayController.java
@@ -0,0 +1,32 @@
+package com.soldesk6F.ondal.useract.payment.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StorePayController {
+
+	
+
+	@GetMapping("/store/pay")
+	public String tryPay() {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		return "/content/pay";
+	}
+	
+	
+	
+	
+	
+	
+}
